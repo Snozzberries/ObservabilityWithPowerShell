ac8da1b01067794763fbeb69ca316c0cf8868c13
Snozz <mike@soule.biz>
2024-03-31 16:56:15 -0700

    Install Monitor placeholder

--- src/ObservabilityWithPowerShell/Private/Install-AzMonitor.ps1
+++ src/ObservabilityWithPowerShell/Private/Install-AzMonitor.ps1
@@ -38,16 +38,38 @@ function Install-AzMonitor {
     }
     process {
         try {
+            Write-Verbose "$prefixVerbose Not currently implemented"
+            Write-Output "$prefixInformation Ensure Azure Arc is installed if not running in Azure https://learn.microsoft.com/en-us/azure/azure-arc/servers/onboard-powershell"
+            Write-Output "$prefixInformation Ensure Azure Monitor Agent is installed https://learn.microsoft.com/en-us/azure/azure-monitor/agents/azure-monitor-agent-manage"
+            <#
             $azure = Get-Cloud
             #Requires -Modules Az.Accounts
-            Connect-AzAccount -DeviceCode
+            #Connect-AzAccount -DeviceCode
+
+            Search-AzGraph
+            resources
+            | where resourceGroup in~ ("RG-ComputeModernOperations")
+            | where type in~ ("microsoft.compute/virtualmachines","microsoft.hybridcompute/machines","microsoft.compute/virtualmachinescalesets","microsoft.azurestackhci/virtualmachines","microsoft.connectedvmwarevsphere/virtualmachines","microsoft.containerservice/managedclusters","microsoft.compute/virtualmachinescalesets/virtualmachines")
+            | where location in~ ("eastus","eastus2","southcentralus","westus2","westus3","australiaeast","southeastasia","northeurope","swedencentral","uksouth","westeurope","centralus","southafricanorth","centralindia","eastasia","japaneast","koreacentral","canadacentral","francecentral","germanywestcentral","italynorth","norwayeast","polandcentral","switzerlandnorth","uaenorth","brazilsouth","israelcentral","qatarcentral","northcentralus","westus","japanwest","westcentralus","southafricawest","australiacentral","australiacentral2","australiasoutheast","koreasouth","southindia","westindia","canadaeast","francesouth","germanynorth","norwaywest","switzerlandwest","ukwest","uaecentral","brazilsoutheast")
+
+            | where type  !in~ ('microsoft.web/serverfarms', 'microsoft.network/loadbalancers')
+                        or (type =~ 'microsoft.web/serverfarms' and tolower(sku.tier) !in~ ('free', 'shared', 'consumption')) 
+                        or (type =~ 'microsoft.network/loadbalancers' and tolower(sku.name) == 'standard')
+            | order by tolower(name) asc
+            | project id,name,location,type,kind,properties,sku
+
+            #Checks for AuthZ, Network, Storage
+
             if($azure){
                 #Requires -Modules Az.Compute
-                Set-AzVMExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName <resource-group-name> -VMName <virtual-machine-name> -Location <location> -TypeHandlerVersion <version-number> -EnableAutomaticUpgrade $true
+                #Set-AzVMExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName <resource-group-name> -VMName <virtual-machine-name> -Location <location> -TypeHandlerVersion <version-number> -EnableAutomaticUpgrade $true
             }else{
                 #Requires -Modules Az.ConnectedMachine
-                New-AzConnectedMachineExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName <resource-group-name> -MachineName <arc-server-name> -Location <arc-server-location> -EnableAutomaticUpgrade
+                #https://learn.microsoft.com/en-us/azure/azure-monitor/agents/azure-monitor-agent-manage?tabs=azure-powershell#virtual-machine-extension-details
+                #Connect-AzConnectedMachine -ResourceGroupName myResourceGroup -Name myMachineName -Location <region>
+                #New-AzConnectedMachineExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName <resource-group-name> -MachineName <arc-server-name> -Location <arc-server-location> -EnableAutomaticUpgrade
             }
+            #>
         }
         catch {
             throw $_
