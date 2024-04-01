f5720025d130873d25ace934ce13190f72e5eef0
Snozz <mike@soule.biz>
2024-03-31 17:43:43 -0700

    Scaffold unit tests

---
+++ src/Tests/Unit/Public/Get-ObsAdds200.Tests.ps1
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'ObservabilityWithPowerShell'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+
+InModuleScope 'ObservabilityWithPowerShell' {
+    #-------------------------------------------------------------------------
+    $WarningPreference = "SilentlyContinue"
+    #-------------------------------------------------------------------------
+    Describe 'Get-ObsAdds200 Public Function Tests' -Tag Unit {
+        
+    }
+} #inModule
