18ad59439173c314b2741ee3d358d96b53a12913
Michael Soule <misoule@sentinel.com>
2024-04-02 06:50:14 -0700

    set return rather than throw for gracefull continue

--- src/ObservabilityWithPowerShell/Private/Install-EventLog.ps1
+++ src/ObservabilityWithPowerShell/Private/Install-EventLog.ps1
@@ -33,62 +33,58 @@ function Install-EventLog {
         $prefixInfo = "[Info][$($MyInvocation.MyCommand.Name)]"
     }
     process {
-        try {
-            Write-Verbose "$prefixVerbose Obtaining Event Log List"
-            $logs=Get-EventLog -List
-            Write-Verbose "$prefixVerbose Obtained $($logs.count) Event Logs"
+        Write-Verbose "$prefixVerbose Obtaining Event Log List"
+        $logs=Get-EventLog -List
+        Write-Verbose "$prefixVerbose Obtained $($logs.count) Event Logs"
 
-            Write-Verbose "$prefixVerbose Checking for existing log"
-            if ($logs.Log -contains $LogName) {
-                Write-Verbose "$prefixVerbose The '$LogName' Log Name already exists"
-                $exists = $true
+        Write-Verbose "$prefixVerbose Checking for existing log"
+        if ($logs.Log -contains $LogName) {
+            Write-Verbose "$prefixVerbose The '$LogName' Log Name already exists"
+            $exists = $true
 
-                Write-Verbose "$prefixVerbose Obtaining Log Sources for $LogName"
-                $existing = (Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\$LogName).pschildname
-                Write-Verbose "$prefixVerbose Obtained $($existing.count) log sources"
+            Write-Verbose "$prefixVerbose Obtaining Log Sources for $LogName"
+            $existing = (Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\$LogName).pschildname
+            Write-Verbose "$prefixVerbose Obtained $($existing.count) log sources"
 
-                Write-Verbose "$prefixVerbose Identifying any overlapping event sources"
-                $compare = Compare-Object $existing $sources -IncludeEqual
-                Write-Verbose "$prefixVerbose $($compare.count) event sources"
-                $overlap = ($compare|Where-Object{$_.SideIndicator -eq "=="}).InputObject
+            Write-Verbose "$prefixVerbose Identifying any overlapping event sources"
+            $compare = Compare-Object $existing $sources -IncludeEqual
+            Write-Verbose "$prefixVerbose $($compare.count) event sources"
+            $overlap = ($compare|Where-Object{$_.SideIndicator -eq "=="}).InputObject
 
-                if ($ContinueOnNewSources -and $overlap) {
-                    Write-Verbose "$prefixVerbose Skipping existing event sources"
-                    $Sources = ($compare|Where-Object{$_.SideIndicator -eq "=>"}).InputObject
-                }
-
-                if ($overlap -and $Sources.Count -eq 0) {
-                    Write-Verbose "$prefixVerbose Found existing event sources, throwing error"
-                    throw "$prefixError $($overlap.count) Log Sources already exist in the $LogName log"
-                }
+            if ($ContinueOnNewSources -and $overlap) {
+                Write-Verbose "$prefixVerbose Skipping existing event sources"
+                $Sources = ($compare|Where-Object{$_.SideIndicator -eq "=>"}).InputObject
             }
 
-            Write-Verbose "$prefixVerbose Registering $($sources.count) event sources"
-            foreach ($source in $sources) {
-                Write-Verbose "$prefixVerbose Registering '$source' event source in '$LogName'"
-                New-EventLog -LogName $LogName -Source $source -ErrorAction Stop
-
-                $log = @{
-                    LogName   = $LogName
-                    EntryType = "Information"
-                    Source    = $source
-                    Category  = 0
-                    EventId   = 1
-                    Message   = "$prefixInfo Implementing $LogName with source $source"
-                }
-                Write-Verbose "$prefixVerbose Writing '$source' event log entry"
-                Write-EventLog @log
-            }
-
-            if (-not $exists) {
-                Write-Verbose "$prefixVerbose First run configuration"
-
-                Write-Verbose "$prefixVerbose Configuring limits for the '$LogName' Event Log"
-                Limit-EventLog -LogName $LogName -OverflowAction $OverflowAction -MaximumSize $Size
+            if ($overlap -and $Sources.Count -eq 0) {
+                Write-Verbose "$prefixVerbose Found existing event sources, exiting function"
+                Write-Error "$prefixError $($overlap.count) Log Sources already exist in the $LogName log"
+                return
             }
         }
-        catch {
-            throw $_
+
+        Write-Verbose "$prefixVerbose Registering $($sources.count) event sources"
+        foreach ($source in $sources) {
+            Write-Verbose "$prefixVerbose Registering '$source' event source in '$LogName'"
+            New-EventLog -LogName $LogName -Source $source -ErrorAction Stop
+
+            $log = @{
+                LogName   = $LogName
+                EntryType = "Information"
+                Source    = $source
+                Category  = 0
+                EventId   = 1
+                Message   = "$prefixInfo Implementing $LogName with source $source"
+            }
+            Write-Verbose "$prefixVerbose Writing '$source' event log entry"
+            Write-EventLog @log
+        }
+
+        if (-not $exists) {
+            Write-Verbose "$prefixVerbose First run configuration"
+
+            Write-Verbose "$prefixVerbose Configuring limits for the '$LogName' Event Log"
+            Limit-EventLog -LogName $LogName -OverflowAction $OverflowAction -MaximumSize $Size
         }
     }
 }
